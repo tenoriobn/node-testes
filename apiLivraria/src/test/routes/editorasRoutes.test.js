5213ea8c8a0f0956a433420fa5bd45edb9f41b1a
Bruno Tenório <tenoriobn@gmail.com>
2024-02-21 16:40:50 -0300

    Hooks

---
+++ apiLivraria/src/test/routes/editorasRoutes.test.js
@@ -0,0 +1,14 @@
+import { afterEach, beforeEach } from "@jest/globals";
+
+import app from "../../app.js"
+
+let server;
+
+beforeEach(() => {
+  const port = 3000;
+  server = app.listen(port);
+});
+
+afterEach(() => {
+  server.close();
+});
